b975b4b53795b1f110b5ed2c4058f73fd38aaf5d
Rafael Camarda <rf.camarda@gmail.com>
2018-10-09 21:51:13 -0300

    add unit tests

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,4 +1,19 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "create deck returns a list" do
+    deck = Cards.create_deck
+    assert is_list(deck)
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
 end
